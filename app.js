c95359c7c6c6ea7d230a2619f87ca27474438366
Pratik Shahi <shahipr0@gmail.com>
2021-03-22 13:54:54 +0530

    connected mongodb to app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+//jshint esversion:6
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+// Database Name
+const dbName = 'naitiveMethodDB';
+
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('documents');
+
+    // the following code examples can be pasted here...
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
